dfd01fea6773e64f98066d3ab2161fbfc943972e
R0mashe4ka <yatakoi4el@yandex.ua>
2019-06-20 11:58:39 +0300

    looks like its working correctly

--- practice1/Startup.cs
+++ practice1/Startup.cs
@@ -1,18 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using AutoMapper;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using practice1.Services;
 using practice1.Helpers;
-using System.Collections.Generic;
-using System.Linq;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace practice1
@@ -33,85 +32,80 @@ namespace practice1
             //services.AddDbContext<Person>(options =>  options.UseSqlServer(connection));
 
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
             services.AddDbContext<DataContext>(x => x.UseInMemoryDatabase("PeopleDB"));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2); ;
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme { In = "header", Description = "Please enter JWT with Bearer into field", Name = "Authorization", Type = "apiKey" });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                    { "Bearer", Enumerable.Empty<string>() },
+                });
+            });
 
             //services.AddAutoMapper();
 
-            var config = new AutoMapper.MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile(new AutoMapperProfile());
-            });
+            //var config = new AutoMapper.MapperConfiguration(cfg =>
+            //{
+            //    cfg.AddProfile(new AutoMapperProfile());
+            //});
 
-            var mapper = config.CreateMapper();
-            services.AddSingleton(mapper);
+            //var mapper = config.CreateMapper();
+            //services.AddSingleton(mapper);
 
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info { Title = "You api title", Version = "v1" });
-                c.AddSecurityDefinition("Bearer",
-                    new ApiKeyScheme
-                    {
-                        In = "header",
-                        Description = "Please enter into field the word 'Bearer' following by space and JWT",
-                        Name = "Authorization",
-                        Type = "apiKey"
-                    });
-                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
 
-                    { "Bearer", Enumerable.Empty<string>() },
 
-                });
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
 
-                var appSettingsSection = Configuration.GetSection("AppSettings");
-                services.Configure<AppSettings>(appSettingsSection);
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            //services.AddAuthentication(x =>
+            //    {
+            //        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    })
+            //    .AddJwtBearer(x =>
+            //    {
+            //        x.Events = new JwtBearerEvents
+            //        {
+            //            OnTokenValidated = context =>
+            //            {
+            //                var personService =
+            //                    context.HttpContext.RequestServices.GetRequiredService<IPersonService>();
+            //                var personId = int.Parse(context.Principal.Identity.Name);
+            //                var person = personService.GetById(personId);
+            //                if (person == null)
+            //                {
+            //                    // return unauthorized if user no longer exists
+            //                    context.Fail("Unauthorized");
+            //                }
+            //                return Task.CompletedTask;
+            //            }
+            //        };
+            //        x.RequireHttpsMetadata = false;
+            //        x.SaveToken = true;
+            //        x.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuerSigningKey = true,
+            //            IssuerSigningKey = new SymmetricSecurityKey(key),
+            //            ValidateIssuer = false,
+            //            ValidateAudience = false
+            //        };
+            //    });
 
-                // configure jwt authentication
-                var appSettings = appSettingsSection.Get<AppSettings>();
-                var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-                services.AddAuthentication(x =>
-                {
-                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(x =>
-                {
-                    x.Events = new JwtBearerEvents
-                    {
-                        OnTokenValidated = context =>
-                        {
-                            var personService = context.HttpContext.RequestServices.GetRequiredService<IPersonService>();
-                            var personId = int.Parse(context.Principal.Identity.Name);
-                            var person = personService.GetById(personId);
-                            if (person == null)
-                            {
-                                // return unauthorized if user no longer exists
-                                context.Fail("Unauthorized");
-                            }
-                            return Task.CompletedTask;
-                        }
-                    };
-                    x.RequireHttpsMetadata = false;
-                    x.SaveToken = true;
-                    x.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
 
-                // configure DI for application services
-                services.AddScoped<IPersonService, PersonService>();
-            });
+
+            // configure DI for application services
+            //services.AddScoped<IPersonService, PersonService>();
         }
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-       
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -129,7 +123,6 @@ namespace practice1
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
             });
 
             if (env.IsDevelopment())
@@ -145,9 +138,9 @@ namespace practice1
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            
 
-            
+
+
         }
     }
 }
